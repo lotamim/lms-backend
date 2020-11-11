f5a5e5579af4c0f51c4e1223abe2b7a1407ccdc3
lotamim <tamim.iubat@gmail.com>
2020-11-11 11:07:32 +0600

    bulid

---
+++ src/main/java/com/newgen/hrm/controller/AuthenticationController.java
@@ -0,0 +1,66 @@
+package com.newgen.hrm.controller;
+
+import com.newgen.hrm.common.Constants;
+import com.newgen.hrm.config.JwtTokenUtil;
+import com.newgen.hrm.model.ApplicationUser;
+import com.newgen.hrm.model.JwtRequest;
+import com.newgen.hrm.service.CustomUserDetailsService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @PostMapping(Constants.LOG_IN)
+    public ResponseEntity<String> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+//        Map<String,String> map = new HashMap<>();
+        JSONObject map = new JSONObject();
+        map.put("username",userDetails.getUsername());
+        map.put("password",userDetails.getPassword());
+        map.put("token",token);
+
+//        return ResponseEntity.ok(new JwtResponse(token));
+        return new ResponseEntity<String>(map.toString(), HttpStatus.OK);
+    }
+
+    @PostMapping(Constants.SIGN_UP)
+    public ResponseEntity<?> saveUser(@RequestBody ApplicationUser applicationUser)  {
+        return  userDetailsService.save(applicationUser);
+//        return ResponseEntity.ok(userDetailsService.save(applicationUser));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
---
+++ src/main/java/com/newgen/hrm/service/CustomUserDetailsService.java
@@ -0,0 +1,65 @@
+package com.newgen.hrm.service;
+/*
+  Note : PLz don't remove this code with any permission.If you need some change plz concern with
+  your Technical Lead or Line manager ;
+  Author : Mehedi Hasan Tamim
+ */
+
+import com.newgen.hrm.common.Constants;
+import com.newgen.hrm.model.ApplicationUser;
+import com.newgen.hrm.model.JwtResponse;
+import com.newgen.hrm.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private static final  String USER_NAME_EXIT = "Username already exits !";
+    private static final  String SUCCESS_MESSAGE = "Data Save Successfully !";
+
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser user = applicationUserRepository.findByUsername(username);
+        List<GrantedAuthority> authorites = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new User(user.getUsername(), user.getPassword(), authorites);
+    }
+
+    public ResponseEntity<?> save(ApplicationUser user) {
+        try {
+            ApplicationUser newUser = new ApplicationUser();
+            int dupUsernameCheck = applicationUserRepository.duplicateCheck(user.getUsername());
+            if (dupUsernameCheck >=1) {
+                return new ResponseEntity<>(USER_NAME_EXIT, HttpStatus.CONFLICT);
+            } else {
+                newUser.setUsername(user.getUsername());
+                newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+                newUser.setEmail(user.getEmail());
+                applicationUserRepository.save(newUser);
+                return new ResponseEntity<>(SUCCESS_MESSAGE, HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+}
