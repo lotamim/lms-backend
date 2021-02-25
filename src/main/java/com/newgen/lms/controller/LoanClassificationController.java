ef2ff35112e4d1dffda72deeac0951284e405a3a
ahsan <ahsan@newgen-bd.com>
2021-02-25 11:35:21 +0600

    Loan Classification setup

--- src/main/java/com/newgen/lms/controller/LoanClassificationController.java
+++ src/main/java/com/newgen/lms/controller/LoanClassificationController.java
@@ -1,12 +1,34 @@
 package com.newgen.lms.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.newgen.lms.service.ChargeService;
+import com.newgen.lms.service.LoanClassificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/loanClassification")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class LoanClassificationController {
+
+    @Autowired
+    private LoanClassificationService loanClassificationService;
+
+    @PostMapping("/saveOrUpdate")
+    public Map saveOrUpdate(@RequestBody Map<String, String> dMap) {
+        return loanClassificationService.saveOrUpdate(dMap);
+    }
+
+    @PostMapping("/delete")
+    public Map delete(@RequestBody Map<String, String> dMap) {
+        return loanClassificationService.delete(dMap);
+    }
+
+    @GetMapping("/list")
+    public Map list() {
+        return loanClassificationService.list();
+    }
+
 
 }
--- src/main/java/com/newgen/lms/service/LoanClassificationService.java
+++ src/main/java/com/newgen/lms/service/LoanClassificationService.java
@@ -1,34 +1,87 @@
 package com.newgen.lms.service;
 
 import com.newgen.lms.common.BaseService;
+import com.newgen.lms.model.LoanClassification;
+import com.newgen.lms.repository.LoanClassificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @Service
 public class LoanClassificationService extends BaseService {
-    public Map saveOrUpdate(Map<String, String> dMap) {
-        try {
-        } catch (Exception ex) {
 
+    private static final String SUCCESS = "Data Save Successful!";
+    private static final String ERROR = "Data not found!";
+    private static final String CLASSIFICATION_NAME_EXIST = "Classification name already exist!";
+    private static final String DELETED = "Data Delete Successful!";
+
+
+    @Autowired
+    private LoanClassificationRepository loanClassificationRepository;
+
+    public Map saveOrUpdate(Map<String, String> dMap) {
+        LoanClassification loanClassification = null;
+        try {
+
+            if (dMap.get("id") != "") {
+                loanClassification = loanClassificationRepository.findByClassificationStageIgnoreCaseAndIdIsNot(dMap.get("classificationStage"), Long.parseLong(dMap.get("id")));
+                if (loanClassification != null) {
+                    return errorMessage(CLASSIFICATION_NAME_EXIST, loanClassification);
+                }
+
+            } else {
+                loanClassification = loanClassificationRepository.findByClassificationStageIgnoreCase(dMap.get("classificationStage"));
+                if (loanClassification != null) {
+                    return errorMessage(CLASSIFICATION_NAME_EXIST, loanClassification);
+                }
+                loanClassification = new LoanClassification();
+            }
+
+            loanClassification.setClassificationStage(dMap.get("classificationStage"));
+            loanClassification.setDuration(Long.parseLong(dMap.get("duration")));
+            loanClassification.setChargeId(Long.parseLong(dMap.get("chargeId")));
+            loanClassification.setType(dMap.get("type"));
+            loanClassification.setPercent(Double.parseDouble(dMap.get("percent")));
+            loanClassification.setRemarks(dMap.get("remarks"));
+
+            loanClassificationRepository.save(loanClassification);
+
+        } catch (Exception ex) {
+            return errorMessage(ERROR, null);
         }
-        return null;
+        return successMessage(SUCCESS, loanClassification);
     }
+
 
     public Map delete(Map<String, String> dMap) {
+        LoanClassification loanClassification = null;
         try {
-        } catch (Exception ex) {
+            if (dMap.get("id") == "") {
+                return errorMessage(ERROR, loanClassification);
+            }
 
+            loanClassification = loanClassificationRepository.findById(Long.parseLong(dMap.get("id"))).get();
+            loanClassification.setDeleted(true);
+            loanClassificationRepository.save(loanClassification);
+
+        } catch (Exception ex) {
+            return errorMessage(ERROR, null);
         }
-        return null;
+        return successMessage(DELETED, loanClassification);
     }
 
-    public Map list(Map<String, String> dMap) {
+    public Map list() {
         try {
+            Map dMap = new LinkedHashMap();
+            List<Map<?, ?>> chargeList = loanClassificationRepository.getList();
+            dMap.put("chargeList", chargeList);
+            return dMap;
         } catch (Exception ex) {
-
+            return errorMessage(ex.getMessage(), null);
         }
-        return null;
     }
 }
