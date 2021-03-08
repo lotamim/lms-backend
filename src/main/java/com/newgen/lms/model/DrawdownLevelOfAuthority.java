c385285e36147853dbbb61be16497f50820f48ef
ahsan <ahsan@newgen-bd.com>
2021-03-08 12:12:55 +0600

    drawdown initil setup

---
+++ src/main/java/com/newgen/lms/model/DrawdownLevelOfAuthority.java
@@ -0,0 +1,31 @@
+package com.newgen.lms.model;
+
+import com.newgen.lms.common.Status;
+import com.newgen.lms.common.Type;
+import com.newgen.lms.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "DrawdownLevelOfAuthority")
+public class DrawdownLevelOfAuthority extends Auditable<String> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long drawdownId;  // foreign key
+    private Long employeeId;  // foreign key
+    private boolean isDeleted = false;
+    private boolean isActive = true;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Type type;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50)
+    private Status status;
+}
---
+++ src/main/java/com/newgen/lms/repository/DrawdownLevelOfAuthorityRepository.java
@@ -0,0 +1,7 @@
+package com.newgen.lms.repository;
+
+import com.newgen.lms.model.DrawdownLevelOfAuthority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrawdownLevelOfAuthorityRepository extends JpaRepository<DrawdownLevelOfAuthority,Long> {
+}
